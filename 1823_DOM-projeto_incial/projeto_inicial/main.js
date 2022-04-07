0243b7179d7d049c0d9604c4576323c30205749d
ThiagoRibas138 <thiago-ribas@outlook.com>
2022-04-07 17:27:01 -0300

    Aplicação de manipulação de DOM

---
+++ 1823_DOM-projeto_incial/projeto_inicial/main.js
@@ -0,0 +1,33 @@
+   import BotaoConclui from "./componentes/concluirTarefa.js";
+   import BotaoDeleta from "./componentes/deletaTarefa.js"
+
+   const criarTarefa = (evento)=> {
+    evento.preventDefault(); 
+    const lista = document.querySelector('[data-list]')   
+    const input = document.querySelector('[data-from-input]');
+    const valor = input.value;
+    
+   
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class ="content"> ${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa)
+    input.value = " "
+ }
+ const novaTarefa = document.querySelector("[data-form-button]");
+
+
+ novaTarefa.addEventListener('click', criarTarefa);
+
+ 
+
+ 
+
+ 
+
+
